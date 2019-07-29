cf2da0cd6871a4f761ae075aad4348dc6cd42d26
Puran Joshi <puran@technogise>
2019-07-29 16:19:38 +0530

    Added test case for 5 off on 100 spent

--- test/model/cart.test.js
+++ test/model/cart.test.js
@@ -1,43 +1,64 @@
-const User = require('../../src/model/user');
 const Cart = require('../../src/model/cart');
 const Product = require('../../src/model/product');
 
 var assert = require('assert');
 
 describe('Cart', function() {
-  describe('#addItem', function(){
+  describe('#addItem()', function(){
     it('should return true when a product is added', function() {
+      //Arrange
       let cart = new Cart();
       let product = new Product('White Bread', 5);
-
+      //Act
       let isAdded = cart.addItem(product, 2);
-
+      //Assert
       assert.equal(true, isAdded);
       
     });
   })
 
-
-  describe('#getPrice', function() {
+  describe('#getPrice()', function() {
     
     it('should return 0 price for empty cart', function(){
-      let user = new User();
+      //Arrange
       let cart = new Cart();
-
-      let result = cart.getPrice(user);
-
-      assert.equal(0, result);
+      //Act
+      let totalPrice = cart.getPrice();
+      //Assert
+      assert.equal(0, totalPrice);
     });
 
-    it('should return 10$ when 2 items of 5$ are added', function(){
-      let user = new User();
+    it('should return 10 when 2 items of 5 are added', function(){
+      //Arrange
       let cart = new Cart();
       let product = new Product('White Bread', 5);
-
       cart.addItem(product, 2);
-      let result = cart.getPrice(user);
+      //Act
+      let totalPrice = cart.getPrice();
+      //Assert
+      assert.equal(10, totalPrice);
+    });
 
-      assert.equal(10, result);
+    it('should return 945 when total cart price is 990 and 5 off on every 100 spent is applied', function(){
+      //Arrange
+      let cart = new Cart();
+      let product = new Product('Earphones', 99);
+      cart.addItem(product, 10);
+      //Act
+      let totalPrice = cart.getPrice();
+      //Assert
+      assert.equal(945, totalPrice);
+    });
+
+    it('should return 90 when total cart price is 90 and 5 off on every 100 spent is applied', function(){
+      //Arrange
+      let cart = new Cart();
+      let product = new Product('Earphones', 90);
+      cart.addItem(product, 1);
+      //Act
+      let totalPrice = cart.getPrice();
+      //Assert
+      assert.equal(90, totalPrice);
     });
   });
 });
