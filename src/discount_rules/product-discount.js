99c018562ae7f53b9620a082aa83c36d507f3b4b
Puran Joshi <puran@technogise>
2019-07-29 17:37:12 +0530

    Fixed the employee discount test case

--- src/discount_rules/product-discount.js
+++ src/discount_rules/product-discount.js
@@ -1,3 +1,14 @@
 module.exports = class ProductOffers {
-  
+  constructor() {
+    this.employeeDiscount = 30;
+  }
+
+  getDiscount(User){
+    switch(true){
+      case User.isEmployee:
+        return this.employeeDiscount;
+      default:
+        return 0;
+    }
+  }
 }
